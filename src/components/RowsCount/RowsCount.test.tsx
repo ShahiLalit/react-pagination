f5819b68444c958d878e0a516c8dcc9cf2ee4517
Lalit Shahi <shahilalit002@gmail.com>
2023-08-24 00:22:17 +0530

    import statement

--- src/components/RowsCount/RowsCount.test.tsx
+++ src/components/RowsCount/RowsCount.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import RowsCount from "./RowsCount";
 
 describe("ShowRowsCount", () => {
